7b93b01f54939ee58e1a8100c383b6ea7790e086
buffoonman <buffoonman@126.com>
2014-06-10 21:14:55 +0800

    test

---
+++ src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: weiguangfu
+ * Date: 14-6-10
+ * Time: 下午9:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hi shit");
+    }
+}
